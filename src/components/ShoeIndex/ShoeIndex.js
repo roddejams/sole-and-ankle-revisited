68f1225c1166c1214d70f801ea4cc592b6269da8
James Rodden <james.rodden@savanta.com>
2021-12-10 11:39:07 +0000

    Exercise 3: Main content

--- src/components/ShoeIndex/ShoeIndex.js
+++ src/components/ShoeIndex/ShoeIndex.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components/macro';
 
-import { WEIGHTS } from '../../constants';
+import { WEIGHTS, QUERIES } from '../../constants';
 
 import Breadcrumbs from '../Breadcrumbs';
 import Select from '../Select';
@@ -14,15 +14,28 @@ const ShoeIndex = ({ sortId, setSortId }) => {
     <Wrapper>
       <MainColumn>
         <Header>
-          <Title>Running</Title>
-          <Select
-            label="Sort"
-            value={sortId}
-            onChange={(ev) => setSortId(ev.target.value)}
-          >
-            <option value="newest">Newest Releases</option>
-            <option value="price">Price</option>
-          </Select>
+          <TitleAndBreadCrumbs>
+            <ResponsiveBreadcrumbs>
+              <Breadcrumbs>
+                <Breadcrumbs.Crumb href="/">Home</Breadcrumbs.Crumb>
+                <Breadcrumbs.Crumb href="/sale">Sale</Breadcrumbs.Crumb>
+                <Breadcrumbs.Crumb href="/sale/shoes">
+                  Shoes
+                </Breadcrumbs.Crumb>
+              </Breadcrumbs>
+            </ResponsiveBreadcrumbs>
+            <Title>Running</Title>
+          </TitleAndBreadCrumbs>
+          <ResponsiveSelect>
+            <Select
+              label="Sort"
+              value={sortId}
+              onChange={(ev) => setSortId(ev.target.value)}
+            >
+              <option value="newest">Newest Releases</option>
+              <option value="price">Price</option>
+            </Select>
+          </ResponsiveSelect>
         </Header>
         <Spacer size={32} />
         <ShoeGrid />
@@ -51,6 +64,24 @@ const Wrapper = styled.div`
 
 const LeftColumn = styled.div`
   flex-basis: 248px;
+
+  @media ${QUERIES.tabletAndUnder} {
+    display: none;
+  }
+`;
+
+const ResponsiveSelect = styled.div`
+  @media ${QUERIES.phoneAndUnder} {
+    display: none;
+  }
+`;
+
+const ResponsiveBreadcrumbs = styled.div`
+  display: none;
+
+  @media ${QUERIES.tabletAndUnder} {
+    display: revert;
+  }
 `;
 
 const MainColumn = styled.div`
@@ -61,11 +92,17 @@ const Header = styled.header`
   display: flex;
   justify-content: space-between;
   align-items: baseline;
+
+  @media ${QUERIES.tabletAndUnder} {
+    align-items: center;
+  }
 `;
 
 const Title = styled.h2`
   font-size: 1.5rem;
   font-weight: ${WEIGHTS.medium};
 `;
+
+const TitleAndBreadCrumbs = styled.div``;
 
 export default ShoeIndex;
