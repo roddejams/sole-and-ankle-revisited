0e3a3df5f8442ce5ff942a400950495c87ca6aa3
James Rodden <james.rodden@savanta.com>
2021-12-10 10:48:54 +0000

    Exercise 1: add breakpoints

--- src/constants.js
+++ src/constants.js
@@ -16,3 +16,15 @@ export const WEIGHTS = {
   medium: 600,
   bold: 800,
 };
+
+const BREAKPOINTS = {
+  phoneMax: 37.5,
+  tabletMax: 59.375,
+  laptopMax: 81.25,
+};
+
+export const Queries = {
+  'laptopAndUnder': `(max-width: ${BREAKPOINTS.laptopMax}rem)`,
+  'tabletAndUnder': `(max-width: ${BREAKPOINTS.tabletMax}rem)`,
+  'phoneAndUnder': `(max-width: ${BREAKPOINTS.phoneMax}rem)`,
+};
